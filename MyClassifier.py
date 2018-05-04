233ac0ea8f3cd31691d16e23a64994e977eb7133
jingyuan tu <jingyuantu@outlook.com>
2018-05-05 00:20:00 +1000

    keep up tomorrow:

--- MyClassifier.py
+++ MyClassifier.py
@@ -1,13 +1,18 @@
 import sys
 
 # Getting arguments
-training_file = sys.argv[1]
-testing_file = sys.argv[2]
+training_file_name = sys.argv[1]
+testing_file_name = sys.argv[2]
 algorithm = sys.argv[3]
 
 # A set of entries for which each entry is made up of another list with corresponding attributes.
 training_entries = []
 testing_entries = []
 
+# Open files
+training_file = open(training_file_name, "r")
+testing_file = open(testing_file_name, "r")
+
 # Reading training entries.
-def readTrainingEntries():
+for line in training_file:
+    training_entries.append(line.rstrip('\n').split(","))
